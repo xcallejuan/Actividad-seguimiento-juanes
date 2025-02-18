28450518ef954238bae025224a06ac866034e2a6
juan arango <juanesarangocalle9@gmail.com>
2025-02-18 11:17:37 -0500

    añadimos el tag

--- app/notebook.py
+++ app/notebook.py
@@ -5,11 +5,16 @@ class Note:
     MEDIUM: str = "MEDIUM"
     LOW: str = "LOW"
 
-    def __init__(self, code: str, title: str, text: str, importance: str):
+    def __init__(self, code: str, title: str, text: str, importance: str ):
         self.code: str = code
         self.title: str = title
         self.text: str = text
         self.importance: str = importance
-        self.creation_date: str = datetime.now()
-        self.tags: str = []
+        self.creation_date: datetime = datetime.now()
+        self.tags: list[str] =[]
+
+    def add_tags(self, tag: str ):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
 
