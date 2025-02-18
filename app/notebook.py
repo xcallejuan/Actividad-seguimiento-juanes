665f501bc068a407eccf90ba6318d496d6cd5239
juan arango <juanesarangocalle9@gmail.com>
2025-02-18 10:57:05 -0500

    Se establece la fecha de creación automáticamente y la Se establece la fecha de creación automáticamente

--- app/notebook.py
+++ app/notebook.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 
 class Note:
-    HIGH = "HIGH"
-    MEDIUM = "MEDIUM"
-    LOW = "LOW"
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: str = datetime.now()
+        self.tags: str = []
+
